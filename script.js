d18f1add54956c6613dca7f8d53f655787dba4a7
botcott <117399920+botcott@users.noreply.github.com>
2026-01-28 23:47:40 +0500

    Use proxy for parse

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Настройки
 const SERVER_IP = '185.97.255.17:1216'; // Deimos
-const SERVER_URL = `https://${SERVER_IP}/status`;
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'; // Public proxy
+const API_URL = `http://${SERVER_IP}/status`;
+const SERVER_URL = PROXY_URL + API_URL;
 const REFRESH_INTERVAL = 5000;
 
 // Элементы
@@ -11,47 +13,47 @@ const serverMap = document.getElementById('server-map');
 const serverIp = document.getElementById('server-ip');
 const serverPreset = document.getElementById('server-preset');
 
-
-// Запрос к API
 async function fetchServerStatus() {
   try {
     const response = await fetch(SERVER_URL, {
       method: 'GET',
       headers: {
-        'Accept': 'application/json'
-      },
+        'Accept': 'application/json',
+        'Origin': window.location.origin
+      }
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP ${response.status}`);
-    }
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
 
     const data = await response.json();
 
     // Обновляем интерфейс
-    playersCount.textContent = `${data.players} из ${data.soft_max_players}` || 0;
+    playersCount.textContent = `${data.players} из ${data.soft_max_players}` || '—';
+    serverRound.textContent = data.round_id || '—';
+    serverMap.textContent = data.map || '—';
+    serverPreset.textContent = data.preset || '—';
+    serverIp.textContent = SERVER_IP;
     serverStatus.textContent = 'Онлайн';
     serverStatus.style.color = '#03da39';
 
-    serverRound.textContent = data.round_id;
-    serverMap.textContent = data.map;
-    serverPreset.textContent = data.preset;
-    serverIp.textContent = SERVER_IP;
-
   } catch (error) {
     console.error('Ошибка запроса:', error);
-    playersCount.textContent = '—';
-    serverRound.textContent = `—`;
-    serverMap.textContent = `—`;
-    serverPreset.textContent = `—`;
-    serverIp.textContent = SERVER_IP;
-    serverStatus.textContent = 'Недоступен';
-    serverStatus.style.color = '#eb2e51';
+    updateOfflineState();
   }
 }
 
-// Запуск и периодическое обновление
+function updateOfflineState() {
+  playersCount.textContent = '—';
+  serverRound.textContent = '—';
+  serverMap.textContent = '—';
+  serverPreset.textContent = '—';
+  serverIp.textContent = SERVER_IP;
+  serverStatus.textContent = 'Недоступен';
+  serverStatus.style.color = '#eb2e51';
+}
+
+// Запуск
 document.addEventListener('DOMContentLoaded', () => {
-  fetchServerStatus(); // первый запрос
+  fetchServerStatus();
   setInterval(fetchServerStatus, REFRESH_INTERVAL);
 });
